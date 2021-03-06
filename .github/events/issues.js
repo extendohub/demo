7125f52a3c966d097f36fb7960a032b6b95be5c0
Jeff McAffer <jeffmcaffer@gmail.com>
2021-03-05 21:40:48 -0800

    Create issues.js

---
+++ .github/events/issues.js
@@ -0,0 +1,7 @@
+const { cache } = context.storage
+const octokit = context.octokit
+const { action, issue } = payload
+cache.set(action, issue.updated_at)
+console.log(cache.get(action))
+const rateLimit = await octokit.rateLimit()
+console.dir(rateLimit) 
