1ab10367528158ec65708d1875513ecd0175ad54
Jeff McAffer <jeffmcaffer@gmail.com>
2021-03-09 13:54:34 -0800

    change issues handler to be a function

--- .github/events/issues.js
+++ .github/events/issues.js
@@ -1,7 +1,9 @@
-const { cache } = context.storage
-const octokit = context.octokit
-const { action, issue } = payload
-cache.set(action, issue.updated_at)
-console.log(cache.get(action))
-const rateLimit = await octokit.rateLimit.get()
-console.dir(rateLimit) 
+({ context, payload }) => {
+  const { cache } = context.storage
+  const octokit = context.octokit
+  const { action, issue } = payload
+  cache.set(action, issue.updated_at)
+  console.log(cache.get(action))
+  const rateLimit = await octokit.rateLimit.get()
+  console.dir(rateLimit) 
+}
