159ce4c8e75318d9320c756d4ad545a7928a8ae9
Jeff McAffer <jeffmcaffer@github.com>
2021-03-10 11:18:52 -0800

    make handler async

--- .github/events/issues.opened.js
+++ .github/events/issues.opened.js
@@ -1,4 +1,4 @@
-({ context, inputs }) => {
+async ({ context, inputs }) => {
   const { repo, issues, issue } = context
   const body = inputs.body || await repo.getContent({ path: '.github/events/ISSUE_REPLY_TEMPLATE.md' })
   return issues.createComment(issue({ body }))
