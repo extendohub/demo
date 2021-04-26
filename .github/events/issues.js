c61f19762435c3efd4d65e61d13a7b16359b3fc1
Jeff McAffer <jeffmcaffer@github.com>
2021-04-26 12:15:46 -0700

    update issue handle

--- .github/events/issues.js
+++ .github/events/issues.js
@@ -1,6 +1,6 @@
 async ({ context, payload }) => {
   const { github, issue, actor } = context
-  console.log(`Detected issue ${payload.action} on issue ${issue.number} by ${actor}`)
+  console.log(`Detected issue ${payload.action} on issue ${issue.issue_number} by ${actor}`)
   const rateLimit = await github.rateLimit.get()
-  console.dir(`The remaining rate limit is ${rateLimit}`) 
+  console.dir(`The remaining rate limit is ${rateLimit.rate.remaining}`) 
 }
