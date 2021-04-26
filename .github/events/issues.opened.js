1a4e83e01cd28f93ac9dc390297595af22d2eef9
Jeff McAffer <jeffmcaffer@github.com>
2021-04-26 15:14:23 -0700

    update event handlers for new api

--- .github/events/issues.opened.js
+++ .github/events/issues.opened.js
@@ -1,5 +1,3 @@
 async ({ context }) => {
-  const { issues, issue } = context
-  const body = 'Hey, **thanks** for opening this great issue. The team will triage it and work with you to address the topic.'
-  return issues.createComment(issue({ body }))
+  return context.issue.createComment({ body: 'Hey, **thanks** for opening this great issue.' })
 }
--- .github/events/pull_request.merged/delete/index.js
+++ .github/events/pull_request.merged/delete/index.js
@@ -1,26 +1,23 @@
 async ({ context, inputs, target, resource }) => {
   const head = target.head
-  if (head.repo.id !== target.base.repo.id) {
-    context.log.info(`Closing PR from fork. Keeping ${head.label}`)
-    return
-  }
-
+  if (head.repo.id !== target.base.repo.id) 
+    return context.info(`Closing PR from fork. Keeping ${head.label}`)
   const branchName = head.ref
   if ((inputs.exclude || []).some((rule) => new RegExp(`^${rule.split('*').join('.*')}$`).test(branchName))) 
-    return context.log.info(`Branch ${branchName} excluded. Keeping ${head.label}`)
+    return context.info(`Branch ${branchName} excluded. Keeping ${head.label}`)
 
   const owner = resource.owner.login
   const repo = resource.name
   const ref = `heads/${branchName}`
   const isMerged = target.merged
   if (!isMerged && config.delete_closed_pr === false) 
-    return context.log.info(`PR was closed but not merged. Keeping ${owner}/${repo}/${ref}`)
+    return context.info(`PR was closed but not merged. Keeping ${owner}/${repo}/${ref}`)
 
   try {
     await context.github.git.deleteRef({ owner, repo, ref })
-    context.log.info(`Successfully deleted ${owner}/${repo}/${ref} which was ${isMerged ? 'merged' : 'closed'}`)
+    context.info(`Successfully deleted ${owner}/${repo}/${ref} which was ${isMerged ? 'merged' : 'closed'}`)
   } catch (e) {
-    context.log.warn(e, `Failed to delete ${owner}/${repo}/${ref}`)
+    context.warn(e, `Failed to delete ${owner}/${repo}/${ref}`)
     throw e
   }
 }
