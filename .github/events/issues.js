9f6435f9e711138d927b169fadcb770675765256
Jeff McAffer <jeffmcaffer@github.com>
2021-04-26 16:41:15 -0700

    dig values out of github ratelimit response

--- .github/events/issues.js
+++ .github/events/issues.js
@@ -1,6 +1,6 @@
 async ({ context, payload }) => {
   const { github, issue, actor } = context
   console.log(`Detected issue ${payload.action} on issue ${issue.number} by ${actor}`)
-  const rateLimit = await github.rateLimit.get()
-  console.dir(`The remaining rate limit is ${rateLimit.rate.remaining}`) 
+  const { data } = await github.rateLimit.get()
+  console.dir(`The remaining rate limit is ${data.rate.remaining}`) 
 }
