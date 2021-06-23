9b77f7afe09e50620252df66e06e0652da385f6a
Jeff McAffer <jeffmcaffer@gmail.com>
2021-06-23 13:36:38 -0700

    Update markedup.js

--- markedup.js
+++ markedup.js
@@ -4,6 +4,16 @@
 // ```
 // Notice how the font color and the style of the subcomponents fits in with the rest of the overall site.
 
+// The general flow of the user interaction is shown in this diagram
+// ```github/mermaid
+// graph TD;
+//     A-->B;
+//     A-->C;
+//     B-->D;
+//     C-->D;
+// ```
+// See how A spawns work which then converges at D. Should be a great experience.
+
 class Component {
   // ... lots of code here
 }
