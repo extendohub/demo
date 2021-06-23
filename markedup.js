1aa79c68076a8832963a74b5de0a338e2349ca20
Jeff McAffer <jeffmcaffer@gmail.com>
2021-06-23 10:38:30 -0700

    Create markedup.js

---
+++ markedup.js
@@ -0,0 +1,7 @@
+// This class makes math easy. Consider the following equation:
+// ```github/math
+// \[ \sum_{i=0}^n i^2 = \frac{(n^2+n)(2n+1)}{6} \]
+// ```
+// With `math` you can do amazing things.
+class Math {
+}
