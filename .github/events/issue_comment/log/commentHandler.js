7a3cf0038eadceb546ec082e86d8d6762d57a4c1
Jeff McAffer <jeffmcaffer@gmail.com>
2021-10-29 14:57:30 -0700

    update to use keyvalue and logging

--- .github/events/issue_comment/log/commentHandler.js
+++ .github/events/issue_comment/log/commentHandler.js
@@ -1,3 +1,6 @@
-({ context, payload }) => {
-  context.log(`handling comment for issue ${payload.issue.number}`)
+({ inputs, api, log }) => {
+  const issue = inputs.payload.issue.number
+  const comment = inputs.payload.comment.id
+  api.keyValue.set('latest', { issue, comment })
+  log.info(`Commented ${comment} on issue ${issue} is the latest`)
 }
