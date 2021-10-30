1f37ea88d9bd258bf99b3b0c962ef6a5e56528a5
Jeff McAffer <jeffmcaffer@gmail.com>
2021-10-29 22:53:13 -0700

    Add a get or two

--- .github/events/issue_comment/log/commentHandler.js
+++ .github/events/issue_comment/log/commentHandler.js
@@ -1,4 +1,8 @@
 async ({ inputs, api, log }) => {
+  const old = await api.keyValue.get('latest')
+  const oldTitle = api.keyValue.get('title')   
+  log.info(`The old latest is ${comment} on issue ${issue} with title ${oldTitle`)
+
   const issue = inputs.payload.issue.number
   const comment = inputs.payload.comment.id
   const value = { issue, comment }
