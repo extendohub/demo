06cc8ed1fa0e33880aa6db76d4d03e3bf7935244
Jeff McAffer <jeffmcaffer@gmail.com>
2021-10-29 17:46:28 -0700

    add async and await

--- .github/events/issue_comment/log/commentHandler.js
+++ .github/events/issue_comment/log/commentHandler.js
@@ -1,6 +1,6 @@
-({ inputs, api, log }) => {
+async ({ inputs, api, log }) => {
   const issue = inputs.payload.issue.number
   const comment = inputs.payload.comment.id
-  api.keyValue.set('latest', { issue, comment })
+  await api.keyValue.set('latest', { issue, comment })
   log.info(`Comment ${comment} on issue ${issue} is the latest`)
 }
