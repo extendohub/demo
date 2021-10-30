da235daa1753ca18b5a33ba74034bd178113c1f1
Jeff McAffer <jeffmcaffer@gmail.com>
2021-10-29 22:27:58 -0700

    remove debugging

--- .github/events/issue_comment/log/commentHandler.js
+++ .github/events/issue_comment/log/commentHandler.js
@@ -2,8 +2,6 @@ async ({ inputs, api, log }) => {
   const issue = inputs.payload.issue.number
   const comment = inputs.payload.comment.id
   const value = { issue, comment }
-  console.log(`******* writing kv **********`)
-  console.dir(value)
   await api.keyValue.set('latest', value)
   log.info(`Comment ${comment} on issue ${issue} is the latest`)
 }
