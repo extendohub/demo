9b379811dd589934f654d7f1f798dac53b257eb4
Jeff McAffer <jeffmcaffer@gmail.com>
2021-10-29 23:30:41 -0700

    reorder

--- .github/events/issue_comment/log/commentHandler.js
+++ .github/events/issue_comment/log/commentHandler.js
@@ -1,10 +1,11 @@
 async ({ inputs, api, log }) => {
+  const issue = inputs.payload.issue.number
+  const comment = inputs.payload.comment.id
+
   const old = await api.keyValue.get('latest')
   const oldTitle = api.keyValue.get('title')   
   log.info(`The old latest is ${comment} on issue ${issue} with title ${oldTitle}`)
 
-  const issue = inputs.payload.issue.number
-  const comment = inputs.payload.comment.id
   const value = { issue, comment }
   await api.keyValue.set('latest', value)
   await api.keyValue.set('title', inputs.payload.issue.title)
