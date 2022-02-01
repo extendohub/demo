1955ec698fefd33250fe1b5400b6ba3fbdcd7de1
Jeff McAffer <jeffmcaffer@gmail.com>
2022-01-31 20:42:36 -0800

    fix payloads reference

--- .github/events/issue_comment/_action/check.js
+++ .github/events/issue_comment/_action/check.js
@@ -1,4 +1,4 @@
 async ({ events }) => {
   // ... do some good checking here
-  console.log(`Everything looks fine with comment: ${events.payload.comment.id} on issue: ${payload.issue.number}`)
+  console.log(`Everything looks fine with comment: ${events.payload.comment.id} on issue: ${events.payload.issue.number}`)
 }
