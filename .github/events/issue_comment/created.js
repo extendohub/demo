86b5f3c4ee29832b029c7f77fe4bbbfb6ebdb1e8
Jeff McAffer <jeffmcaffer@github.com>
2022-02-01 05:13:11 +0000

    move issue comment created to edited

--- .github/events/issue_comment/created.js
+++
@@ -1,4 +0,0 @@
-async ({ events }) => {
-  // ... do some good checking here
-  console.log(`Everything looks fine with comment: ${events.payload.comment.id} on issue: ${events.payload.issue.number}`)
-}
