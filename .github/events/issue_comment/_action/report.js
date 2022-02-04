a6c1dffec399ec93e5db90394ddc9ecd47169d40
Jeff McAffer <jeffmcaffer@github.com>
2022-02-04 17:14:23 +0000

    make reporting message to be more consistent

--- .github/events/issue_comment/_action/report.js
+++ .github/events/issue_comment/_action/report.js
@@ -5,11 +5,11 @@ async ({ events, helpers, log }) => {
   const comment = payload.comment.id
 
   const old = await keyValue.get('latest') || {}
-  const oldTitle = await keyValue.get('title') || {}
+  const oldTitle = await keyValue.get('title') || ''
   log.info(`The old latest is ${old.comment} on issue ${old.issue} with title "${oldTitle}"`)
 
   const value = { issue, comment }
   await keyValue.set('latest', value)
   await keyValue.set('title', payload.issue.title)
-  log.info(`Comment ${comment} on issue ${issue} is the new latest`)
+  log.info(`The new latest is ${comment} on issue ${issue} with title ${payload.issue.title}`)
 }
