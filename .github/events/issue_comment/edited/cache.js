30e7b9d210d3c1048c46f92500fa10f329d96060
Jeff McAffer <jeffmcaffer@github.com>
2022-02-04 03:43:13 +0000

    add cache example (issue_comment/edited)

---
+++ .github/events/issue_comment/edited/cache.js
@@ -0,0 +1,14 @@
+async ({ events, helpers, log }) => {
+  const cachedValue = await helpers.cache.get('currentId')
+  log.info(`The cached comment is: ${cachedValue}`)
+  await helpers.cache.set('currentId', events.payload.comment.id, 2)
+  const newValue = await helpers.cache.get('currentId')
+  log.info(`The new comment is: ${newValue}`)
+  await delay(2500)
+  const delayedValue = await helpers.cache.get('currentId')
+  log.info(`The delayed comment is: ${delayedValue}`)
+}
+
+function delay(time) {
+  return Promise(resolve => setTimeout(() => resolve(), time))
+} 
