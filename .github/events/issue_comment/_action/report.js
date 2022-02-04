aa265f93215fb0ae4b2c9bbb18fd43606d08ee5f
Jeff McAffer <jeffmcaffer@github.com>
2022-02-04 17:15:40 +0000

    add quotes

--- .github/events/issue_comment/_action/report.js
+++ .github/events/issue_comment/_action/report.js
@@ -11,5 +11,5 @@ async ({ events, helpers, log }) => {
   const value = { issue, comment }
   await keyValue.set('latest', value)
   await keyValue.set('title', payload.issue.title)
-  log.info(`The new latest is ${comment} on issue ${issue} with title ${payload.issue.title}`)
+  log.info(`The new latest is ${comment} on issue ${issue} with title "${payload.issue.title}"`)
 }
