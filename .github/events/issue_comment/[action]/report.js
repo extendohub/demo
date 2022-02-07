72ce81ea87597e0e6302d9c99a6b0d0465078f14
Jeff McAffer <jeffmcaffer@github.com>
2022-02-07 15:29:05 +0000

    use nextjs routing

--- .github/events/issue_comment/[action]/report.js
+++ .github/events/issue_comment/[action]/report.js
@@ -1,15 +1,16 @@
-async ({ events, helpers, log }) => {
+async ({ events, context, helpers, log }) => {
   const { keyValue } = helpers
   const { payload } = events
   const issue = payload.issue.number
   const comment = payload.comment.id
 
+  log.info(`Processing ${context.config.params.action} action on issue #${issue}` )
   const old = await keyValue.get('latest') || {}
   const oldTitle = await keyValue.get('title') || ''
-  log.info(`The old latest is ${old.comment} on issue ${old.issue} with title "${oldTitle}"`)
+  log.info(`The old latest is ${old.comment} on issue #${old.issue} with title "${oldTitle}"`)
 
   const value = { issue, comment }
   await keyValue.set('latest', value)
   await keyValue.set('title', payload.issue.title)
-  log.info(`The new latest is ${comment} on issue ${issue} with title "${payload.issue.title}"`)
+  log.info(`The new latest is ${comment} on issue #${issue} with title "${payload.issue.title}"`)
 }
